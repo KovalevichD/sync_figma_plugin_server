a04c091db176ecb060e3b6de35416b6aeda23cdd
dmitry <kovalevich.demi@gmail.com>
2022-07-13 15:20:22 +0300

    upload images with splitted payload

--- src/routes/uploadImages.router.js
+++ src/routes/uploadImages.router.js
@@ -10,14 +10,25 @@ router.route('/:baseId/:tableId').post(
     catchErrors(async (req, res) => {
         const payload = req.body;
         const {baseId, tableId} = req.params;
-        const apiKey = req.get(AUTH_HEADER)
-        const response = await axiosInstance.patch(`${baseId}/${tableId}`, payload, {headers: {"Authorization": `Bearer ${apiKey}`}});
-        const uploadResult = response.data.records;
+        const apiKey = req.get(AUTH_HEADER);
+        const result = [];
+        let receivedRecords = 0;
+        let updatedRecords = 0;
 
-        if (!uploadResult.length) {
+        payload.forEach(el => receivedRecords += el.records.length);
+
+        for (let i = 0; i < payload.length; i++) {// todo make 5 req/sec
+            const response = await axiosInstance.patch(`${baseId}/${tableId}`, payload[i], {headers: {"Authorization": `Bearer ${apiKey}`}});
+            const updatedRecordsResult = response.data.records;
+
+            result.push(updatedRecordsResult);
+            updatedRecords += updatedRecordsResult.length
+        }
+
+        if (!result.length) {
             throw new ErrorHandler(NOT_FOUND, 'Images were not loaded');
         } else {
-            res.status(OK).send(uploadResult);
+            res.status(OK).send(`${updatedRecords}/${receivedRecords} records updated`);
         }
 
     })
